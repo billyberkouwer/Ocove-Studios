3a7bb0d625d1a926d0ac37030395ef317c2ca2eb
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-08-13 23:19:33 +0100

    Update

---
+++ src/HomeSection.js
@@ -0,0 +1,16 @@
+import logo from './assets/company-name.png';
+
+const HomeSection = () => {
+    return (
+        <div>
+            <img src={logo} alt="company-name" className="company-name"></img>
+            <div className="tag-container">
+                <h1 className="company-tag">The Creative Digital Services Company</h1>
+            </div>
+            <div className="header-imgs-container"></div>
+            <div className="header-text-container"></div>
+        </div>
+     );
+}
+ 
+export default HomeSection;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <ul className="nav-links">
+                <div className="horizontal-line"></div>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Projects</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Contact</a></li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Navbar;
