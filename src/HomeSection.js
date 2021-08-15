96a33635082ee2cf4b25b9790374db952de0d384
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-08-15 23:46:20 +0100

    fading bg nav

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -1,10 +1,11 @@
 import logo from './assets/company-name.png';
 
 const HomeSection = () => {
+
     return (
         <div>
-            <img src={logo} alt="company-name" className="company-name"></img>
-            <div className="tag-container">
+            <img src={logo} alt="company-name" className="company-name" id="logo" draggable="false"></img>
+            <div className="tag-container no-select">
                 <h1 className="company-tag">The Creative Digital Services Company</h1>
             </div>
             <div className="header-imgs-container"></div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
+    const [NavColor, setNavColor] = useState('');
+    const bg1 = 'rgba(255, 255, 255, 0)';
+    const bg2 = 'rgba(255, 255, 255, 1)';
+
+    window.onscroll = function() {changeColor()};
+
+    function changeColor() {
+            if (window.scrollY <= 30) {
+                setNavColor(bg1);
+                console.log(NavColor)
+            } else {
+                setNavColor(bg2);
+                console.log(NavColor)
+            };
+    };
+
     return ( 
-        <div className="navbar">
+        <div className="navbar" style={{backgroundColor: NavColor}}>
             <ul className="nav-links">
                 <div className="horizontal-line"></div>
                 <li><a href="/">Home</a></li>
