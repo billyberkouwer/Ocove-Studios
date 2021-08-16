ef8e35e7668d19c3077e7de4a1fddd7789129bdb
billyberkouwer <billyberkouwer@live.com>
2021-08-16 13:14:07 +0100

    font-update

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -9,7 +9,7 @@ const HomeSection = () => {
                 <h1 className="company-tag">The Creative Digital Services Company</h1>
             </div>
             <div className="header-imgs-container"></div>
-            <div className="header-text-container"></div>
+            <div className="header-text-container"><p>“Proin gravida nibh vel auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit”</p></div>
         </div>
      );
 }
