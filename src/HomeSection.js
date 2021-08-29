b9581e184eeaff720466b3378519b0983858a07e
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-08-29 22:51:14 +0100

    update

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -2,14 +2,17 @@ import logo from './assets/company-name.png';
 
 const HomeSection = () => {
 
+    const companyTag = "The Creative Digital Services Company";
+    const loremIpsum = "Proin gravida nibh vel auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit";
+
     return (
         <div>
             <img src={logo} alt="company-name" className="company-name" id="logo" draggable="false"></img>
             <div className="tag-container no-select">
-                <h1 className="company-tag">The Creative Digital Services Company</h1>
+                <h1 className="company-tag">{companyTag}</h1>
             </div>
             <div className="header-imgs-container"></div>
-            <div className="header-text-container"><p>“Proin gravida nibh vel auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit”</p></div>
+            <div className="header-text-container"><p>"{loremIpsum}"</p></div>
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
 
     const [NavColor, setNavColor] = useState('');
+
     const bg1 = 'rgba(255, 255, 255, 0)';
     const bg2 = 'rgba(255, 255, 255, 1)';
 
-    window.onscroll = function() {changeColor()};
-
-    function changeColor() {
-            if (window.scrollY <= 30) {
+    useEffect(() => {
+        window.onscroll = changeColor();
+        function changeColor() {
+            setInterval(function() {
+            if (window.scrollY < 40) {
                 setNavColor(bg1);
             } else {
                 setNavColor(bg2);
-            };
-    };
+            };}, 500)
+        };
+    }, []);
+
+    const sectionNames = [
+        'Home',
+        'Projects',
+        'Services',
+        'About',
+        'Contact'
+    ];
+
+    const navLinks = sectionNames.map((sectionNames) => 
+        <li><a href='/'>{sectionNames}</a></li>
+    );
 
     return ( 
         <div className="navbar" style={{backgroundColor: NavColor}}>
             <ul className="nav-links">
                 <div className="horizontal-line"></div>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Projects</a></li>
-                <li><a href="/">Services</a></li>
-                <li><a href="/">About</a></li>
-                <li><a href="/">Contact</a></li>
+                {navLinks}
             </ul>
         </div>
      );
