1a8c7c13eda587ca80c2e5556d77f048e144ba43
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-09-01 11:05:36 +0100

    mapped-objects

--- src/ProjectSection.js
+++ src/ProjectSection.js
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+
 const ProjectSection = () => {
 
     const loremIpsum = "Proin gravida nibh vel auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit";
 
+    const projectObjects = [
+        {
+            'title': 'Chappel Galleries',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': 'John Hounam Fine Art',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': 'Helen Howard Ceramics',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': 'Isabel Knowland Fine Art',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': 'Carluca Decorators',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': 'Dog Treats',
+            'text': {loremIpsum},
+            'images': ''
+        },
+        {
+            'title': '3D',
+            'text': {loremIpsum},
+            'images': ''
+        }
+    ];
+
+    const projectTitles = [];
+
+    for (let i = 0; i < projectObjects.length; i++) {
+        const element = projectObjects[i];
+        projectTitles.push(element.title);
+    };
+
+    const projectTitleLinks = projectTitles.map((projectTitles) => 
+       <li>{projectTitles}</li>
+    )
     return ( 
         <div className="section-container">
             <div className="section-title no-select">
@@ -10,13 +60,7 @@ const ProjectSection = () => {
             <div className="project-container horizontal-center">
                 <div className="project-menu">
                     <ul>
-                        <li>Chappel Galleries</li>
-                        <li>John Hounam Fine Art</li>
-                        <li>Helen Howard Ceramics</li>
-                        <li>Isabel Knowland Fine Art</li>
-                        <li>Carluca Decorators</li>
-                        <li>Dog Treats</li>
-                        <li>3D</li>
+                        {projectTitleLinks}
                     </ul>
                 </div>
                 <div className="project-window">
