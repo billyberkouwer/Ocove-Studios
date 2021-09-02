1ea2eb8213a3d17331338bf84d5887da7761a31f
Matt Congdon <mattcongdon95@gmail.com>
2021-09-02 19:32:12 +0100

    box rotation animation

--- src/ServiceIcon.js
+++ src/ServiceIcon.js
@@ -1,17 +1,24 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 
-function ServiceIcon () {
-    return(
-        <Canvas>
-            <mesh>
+const ServiceIcon = () => {
+    const icon = useRef(null);
+    useFrame(()=> (icon.current.rotation.y += 0.01));
+    return (
+            <mesh ref={icon}>
                 <boxBufferGeometry attach="geometry" args={[3,3,3]}/>
                 <meshStandardMaterial attach="material" />
             </mesh>
-        </Canvas>
-    )             
-}
+        );             
+    }
 
-export default ServiceIcon;
-                    
+const IconCanvas = () => {
+    return (
+        <Canvas>
+            <ServiceIcon />
+        </Canvas>
+        )
+    }
+
+export default IconCanvas;
 
