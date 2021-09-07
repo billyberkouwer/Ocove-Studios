5a59615c4f3d5acbac34503a30977c31f8375919
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-09-07 12:47:24 +0100

    services section update

--- src/ServiceIcon.js
+++ src/ServiceIcon.js
@@ -3,23 +3,25 @@ import { Canvas, useFrame } from '@react-three/fiber';
 
 const ServiceIcon = () => {
     const icon = useRef(null);
-    useFrame(()=> (icon.current.rotation.y += 0.01));
+    useFrame(() => (icon.current.rotation.y += 0.01));
     return (
             <mesh ref={icon}>
-                <boxBufferGeometry attach="geometry" args={[3,3,3]}/>
-                <meshStandardMaterial attach="material" />
+                <boxBufferGeometry attach="geometry" args={[3,3,3]} />
+                <meshStandardMaterial attach="material" color="#dee2ff" />
             </mesh>
         );             
     }
 
-function IconCanvas () {
-    return (
-        <>
-        <Canvas>
-            <ServiceIcon />
-        </Canvas>
-        </>
-        )
+    function IconCanvas () {
+        return (
+            <>
+            <Canvas>
+                <pointLight position={[20, 20, 10]} />
+                <pointLight position={[-20, -20, 10]} />
+                <ServiceIcon />
+            </Canvas>
+            </>
+            )
     }
 
 export default IconCanvas;
--- src/ServiceSection.js
+++ src/ServiceSection.js
@@ -7,8 +7,8 @@ const ServiceSection = () => {
             <div className="section-title no-select">
                 <h1>Services</h1>
             </div>
-            <div className="services-container vertical-center">
-                <div className="service-icon-container horizontal-center">
+            <div className="services-container">
+                <div className="service-icon-container">
                     <div className="service-icon web-icon">
                         <ServiceIcon/>
                     </div>
@@ -16,7 +16,7 @@ const ServiceSection = () => {
                         <h2>website development</h2>
                     </div>
                 </div>
-                <div className="service-icon-container horizontal-center">
+                <div className="service-icon-container">
                     <div className="service-icon 3d-icon"> 
                         <ServiceIcon/>
                     </div>
@@ -24,7 +24,7 @@ const ServiceSection = () => {
                         <h2>3d modelling & rendering</h2>
                     </div>
                 </div>
-                <div className="service-icon-container horizontal-center">
+                <div className="service-icon-container">
                     <div className="service-icon video-icon">
                         <ServiceIcon/>
                     </div>
@@ -32,12 +32,12 @@ const ServiceSection = () => {
                         <h2>video production</h2>
                     </div>
                 </div>
-            </div>
+            </div> 
             <div className="enquire-button-container horizontal-center vertical-center">
                 <div className="enquire-button">
                     <button>ENQUIRE</button>
                 </div>
-            </div> 
+            </div>
         </div>
         );      
 
