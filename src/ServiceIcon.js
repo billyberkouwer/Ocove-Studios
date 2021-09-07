1f5f359f91f613c3044b648ab2fc4a4592fff6ed
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-09-07 13:15:31 +0100

    updated services section

--- src/ServiceIcon.js
+++ src/ServiceIcon.js
@@ -5,23 +5,23 @@ const ServiceIcon = () => {
     const icon = useRef(null);
     useFrame(() => (icon.current.rotation.y += 0.01));
     return (
-            <mesh ref={icon}>
-                <boxBufferGeometry attach="geometry" args={[3,3,3]} />
-                <meshStandardMaterial attach="material" color="#dee2ff" />
-            </mesh>
-        );             
-    }
+        <mesh ref={icon} >
+            <boxBufferGeometry attach="geometry" args={[3,3,3]} />
+            <meshStandardMaterial attach="material" color="#dee2ff" />
+        </mesh>
+    );             
+}
 
-    function IconCanvas () {
-        return (
-            <>
-            <Canvas>
-                <pointLight position={[20, 20, 10]} />
-                <pointLight position={[-20, -20, 10]} />
-                <ServiceIcon />
-            </Canvas>
-            </>
-            )
-    }
+function IconCanvas () {
+    return (
+        <>
+        <Canvas>
+            <pointLight position={[20, 20, 10]} />
+            <pointLight position={[-20, -20, 10]} />
+            <ServiceIcon />
+        </Canvas>
+        </>
+    )
+}
 
 export default IconCanvas;
