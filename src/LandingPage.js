cd7a1e2e218b33ec5499ef86392679817a25557e
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-09-23 11:15:52 +0100

    landing-page-updates

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,9 +4,10 @@ const LandingPage = () => {
 
     return ( 
         <div className="section-container">
+            <div className="landing-page-bg"></div>
             <div className="landing-page-container">
                 <div className="brand-name">Ocove Studios</div>
-                <div style={{width: "50%", margin: "0 25%"}} className="horizontal-line"></div>
+                <div style={{width: "50%", margin: "0 25%", height: "2px"}} className="horizontal-line"></div>
                 <div className="brand-tag">The Creative Digital Services Specialists</div>
             </div>
         </div>
