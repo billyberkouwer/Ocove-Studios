5e00ff67d1835e9cbf866c28e2d9d8c3467edba4
Billy <76756401+billyberkouwer@users.noreply.github.com>
2021-09-29 17:42:40 +0100

    landing page update

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,4 +1,22 @@
+import { useState, useEffect, useRef } from "react";
+
 const LandingPage = () => {
+
+    const [fade, setFade] = useState('0');
+    let scrollpos = useRef(0);
+
+    useEffect(() => {
+        setInterval(function() {
+            const fadeStart = 120;
+            scrollpos.current = window.scrollY;
+            if (scrollpos.current < fadeStart) {
+                setFade('0')
+            } else {
+                console.log(scrollpos.current)
+                setFade('1');
+            };
+        }, 200)
+    }, [])
 
     return ( 
         <div className="section-container-scroll">
@@ -8,7 +26,7 @@ const LandingPage = () => {
                 <div style={{width: "50%", margin: "0 25%", height: "2px"}} className="horizontal-line"></div>
                 <div className="brand-tag no-select">The Creative Digital Services Specialists</div>
             </div>
-            <div className="info-container">
+            <div className="info-container" style={{opacity: fade}}>
                 <p>Morbi lobortis eros a libero placerat aliquam. Nulla nec nisi id neque fermentum vestibulum. Praesent sit amet arcu id sapien sodales iaculis malesuada sed massa. Donec lacinia at turpis ac tincidunt. Nulla euismod fringilla urna, vel vehicula tellus commodo a. Proin dui ligula, ullamcorper id velit id, vulputate sollicitudin neque. Donec id luctus mi. Sed sed ex vitae urna scelerisque mollis. Aenean sollicitudin, felis id mollis auctor, dui mi eleifend ex, quis varius neque mi in arcu.</p>
             </div>
         </div>
