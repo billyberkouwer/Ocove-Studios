5b35a2e3e94cb485b7c0c800cce456cd372676e5
Matt Congdon <mattcongdon95@gmail.com>
2021-11-05 14:50:02 +0000

    new homepage structure & fonts

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,36 +1,57 @@
-import { useState, useEffect, useRef } from "react";
+// import { useState, useEffect, useRef } from "react";
 
 const LandingPage = () => {
 
-    const [fade, setFade] = useState('0');
-    let scrollpos = useRef(0);
+//     const [fade, setFade] = useState('0');
+//     let scrollpos = useRef(0);
 
-    useEffect(() => {
-        setInterval(function() {
-            const fadeStart = 100;
-            scrollpos.current = window.scrollY;
-            if (scrollpos.current < fadeStart) {
-                setFade('0')
-            } else {
-                console.log(scrollpos.current)
-                setFade('1');
-            };
-        }, 100)
-    }, [])
+//     useEffect(() => {
+//         setInterval(function () {
+//             const fadeStart = 100;
+//             scrollpos.current = window.scrollY;
+//             if (scrollpos.current < fadeStart) {
+//                 setFade('0')
+//             } else {
+//                 console.log(scrollpos.current)
+//                 setFade('1');
+//             };
+//         }, 100)
+//     }, [])
 
-    return ( 
-        <div className="section-container-scroll">
-            <div className="landing-page-bg no-select"></div>
-            <div className="landing-page-container">
-                <div className="brand-name no-select">Ocove Studios</div>
-                <div style={{width: "50%", margin: "0 25%", height: "2px"}} className="horizontal-line"></div>
-                <div className="brand-tag no-select">The Creative Digital Services Specialists</div>
-            </div>
-            <div className="info-container" style={{opacity: fade}}>
-                <p>Morbi lobortis eros a libero placerat aliquam. Nulla nec nisi id neque fermentum vestibulum. Praesent sit amet arcu id sapien sodales iaculis malesuada sed massa. Donec lacinia at turpis ac tincidunt. Nulla euismod fringilla urna, vel vehicula tellus commodo a. Proin dui ligula, ullamcorper id velit id, vulputate sollicitudin neque. Donec id luctus mi. Sed sed ex vitae urna scelerisque mollis. Aenean sollicitudin, felis id mollis auctor, dui mi eleifend ex, quis varius neque mi in arcu.</p>
-            </div>
+    return (
+        <div className="home">
+            <section className="home-1 wrapper">
+                <div className="brand-name wrapper">
+                    <div className="brand-name-line-1 no-select">OCOVE</div>
+                    <div className="brand-name-line-2 no-select">STUDIOS</div>
+                </div>
+            </section>
+
+            <section className="home-2 wrapper">
+                <div className="info-container-1">
+                    <div>
+                        <p>We are a UK-based freelance collective specialising in website development,
+                            video production and 3D visualisation, providing innovative digital products
+                            for creatives, brands and businesses.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section className="home-3 wrapper">
+                <div className="home-3-left">
+                    <div className="info-container-2">
+                        <p>Working as a collective allows us to bring our best creative assets to projects.
+                            Collaborating as individuals, we can make things together that we couldn't alone.</p>
+                    </div>
+                </div>
+                <div className="home-3-right">
+                    <div className="particle-container">
+                    </div>
+                    <button className="view-projects-button"> SEE OUR WORK </button>
+                </div>
+            </section>
         </div>
-     );
+    );
 }
- 
+
 export default LandingPage;
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,10 +8,9 @@ const Navbar = () => {
             <ul className="nav-links">
                 <img alt="Ocove logo" src={logo} className="ocove-logo"></img>
                 <div className="horizontal-line"></div>
-                <li key="homepage"><Link to="/">Home</Link></li>
-                <li key="projects"><Link to="/projects">Projects</Link></li>
-                <li key="services"><Link to="/services">Services</Link></li>
-                <li key="contact"><Link to="/contact">Contact</Link></li>
+                <li key="homepage"><Link to="/">HOME</Link></li>
+                <li key="projects"><Link to="/projects">PROJECTS</Link></li>
+                <li key="services"><Link to="/services">SERVICES</Link></li>
             </ul>
         </div>
      );
