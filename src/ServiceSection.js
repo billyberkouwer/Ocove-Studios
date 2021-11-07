13c4b2aa753d49bf429c0883eac4c7f5e88b87a4
Matt Congdon <mattcongdon95@gmail.com>
2021-11-07 21:51:35 +0000

    services form structure

--- src/ServiceSection.js
+++ src/ServiceSection.js
@@ -8,6 +8,9 @@ const ServiceSection = () => {
     return (
         <div className="services">
             <section className="services-1 wrapper">
+                <div className="header-container no-select">
+                    <h2>What can we do for you?</h2>
+                </div>
                 <div className="services-container">
                     <div className="service-icon-container">
                         <div className="service-icon web-icon">
@@ -16,7 +19,7 @@ const ServiceSection = () => {
                         this issue */}
                         </div>
                         <div className="service-name">
-                            <h2>website development</h2>
+                            <h4>website development</h4>
                         </div>
                     </div>
                     <div className="service-icon-container">
@@ -26,7 +29,7 @@ const ServiceSection = () => {
                         this issue */}
                         </div>
                         <div className="service-name">
-                            <h2>3d modelling & rendering</h2>
+                            <h4>3d modelling & rendering</h4>
                         </div>
                     </div>
                     <div className="service-icon-container">
@@ -36,18 +39,60 @@ const ServiceSection = () => {
                         this issue */}
                         </div>
                         <div className="service-name">
-                            <h2>video production</h2>
+                            <h4>video production</h4>
                         </div>
                     </div>
                 </div>
-              
+
             </section>
 
             <section className="services-2 wrapper">
+                <div className="header-container no-select">
+                    <h2>Tell us more</h2>
+                </div>
+                <div className="service-options-header">
+                    <h4>Service:</h4>
+                </div>
 
+                <div className="service-options-container">
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">Web Development</label></div>
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">Web Design</label></div>
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">Branding</label></div>
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">3D Visuals</label></div>
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">Photo Work</label></div>
+                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
+                        <label for="service-checkbox">Video Work</label></div>
+                </div>
+                <div className="form-container">
+                    <div className="form">
+                        <form name="contact" method="POST" data-netlify="true">
+                            <p>
+                                <input type="text" name="name" placeholder="Name" />
+                            </p>
+                            <p>
+                                <input type="email" name="email" placeholder="Email" />
+                            </p>
+                            <p>
+                                <textarea name="message" placeholder="Message"></textarea>
+                            </p>
+                        </form>
+                    </div>
+                    <div className="form-button">
+                        <button type="submit">SEND</button>
+                    </div>
+                </div>
             </section>
 
-            <section className="services-3 wrapper">
+            <section className="services-3">
+                <div className="header-container no-select">
+                    <h2>Or contect us directly</h2>
+
+                </div>
 
             </section>
 
