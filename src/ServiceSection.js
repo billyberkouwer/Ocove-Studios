d5a5d8108b8141db28fb9ad424203a2a39932c05
Matt Congdon <mattcongdon95@gmail.com>
2021-11-08 21:11:48 +0000

    updated service form structure

--- src/ServiceSection.js
+++ src/ServiceSection.js
@@ -19,7 +19,7 @@ const ServiceSection = () => {
                         this issue */}
                         </div>
                         <div className="service-name">
-                            <h4>website development</h4>
+                            <h4>web development</h4>
                         </div>
                     </div>
                     <div className="service-icon-container">
@@ -29,7 +29,17 @@ const ServiceSection = () => {
                         this issue */}
                         </div>
                         <div className="service-name">
-                            <h4>3d modelling & rendering</h4>
+                            <h4>3d visuals</h4>
+                        </div>
+                    </div>
+                    <div className="service-icon-container">
+                        <div className="service-icon video-icon">
+                            <CameraIcon />    {/* 3D and web icons produce a glitch on page
+                        refresh and direct url access. Camera works fine. We should investigate
+                        this issue */}
+                        </div>
+                        <div className="service-name">
+                            <h4>branding</h4>
                         </div>
                     </div>
                     <div className="service-icon-container">
@@ -43,50 +53,23 @@ const ServiceSection = () => {
                         </div>
                     </div>
                 </div>
-
-            </section>
-
-            <section className="services-2 wrapper">
-                <div className="header-container no-select">
-                    <h2>Tell us more</h2>
-                </div>
-                <div className="service-options-header">
-                    <h4>Service:</h4>
-                </div>
-
-                <div className="service-options-container">
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">Web Development</label></div>
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">Web Design</label></div>
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">Branding</label></div>
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">3D Visuals</label></div>
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">Photo Work</label></div>
-                    <div className="service-option"><input type="checkbox" id="service-checkbox"></input>
-                        <label for="service-checkbox">Video Work</label></div>
-                </div>
                 <div className="form-container">
                     <div className="form">
                         <form name="contact" method="POST" data-netlify="true">
-                            <p>
-                                <input type="text" name="name" placeholder="Name" />
-                            </p>
-                            <p>
-                                <input type="email" name="email" placeholder="Email" />
-                            </p>
-                            <p>
-                                <textarea name="message" placeholder="Message"></textarea>
-                            </p>
+                            <input type="text" name="name" placeholder="Name" />
+                            <input type="email" name="email" placeholder="Email" />
+                            <textarea name="message" placeholder="Message"></textarea>
                         </form>
                     </div>
                     <div className="form-button">
                         <button type="submit">SEND</button>
                     </div>
                 </div>
+
             </section>
+
+
+
 
             <section className="services-3">
                 <div className="header-container no-select">
