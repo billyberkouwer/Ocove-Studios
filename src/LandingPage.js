ad63ea58adc27faac85fb1ec5563aff33514f699
Matt Congdon <mattcongdon95@gmail.com>
2021-11-22 15:55:24 +0000

    home page animation test

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -12,23 +12,78 @@ const LandingPage = () => {
     const e = useRef();
     const q = gsap.utils.selector(e);
 
+    const tl = gsap.timeline({ paused: true })
+
+    const bgVideo = useRef();
+
+    const brandName1 = useRef();
+    const brandName2 = useRef();
+
+    const infoLine1 = useRef();
+    const infoLine2 = useRef();
+    const infoLine3 = useRef();
+    const infoLine4 = useRef();
+    const infoLine5 = useRef();
+
+    const infoBlock = useRef();
+
     gsap.registerPlugin(ScrollTrigger);
 
     useEffect(() => {
-        gsap.from(q(".brand-name-line-1"), {
-            y: 250,
-            duration: 0.5
-        })
-        gsap.from(q(".brand-name-line-2"), {
-            y: -250,
-            duration: 0.5
+
+        const infoLines = [
+            infoLine1.current,
+            infoLine2.current,
+            infoLine3.current,
+            infoLine4.current,
+            infoLine5.current,
+        ]
+
+        gsap.to(bgVideo.current, {
+            opacity: 0,
+            scrollTrigger: {
+                trigger: ".home-2",
+                scrub: true
+            }
         })
 
-        ScrollTrigger.create({
-            trigger: ".home-2",
-            pin: true,
-            start: "top top",
-            end: "+=30%"
+        gsap.from(brandName1.current, {
+            y: 90,
+            opacity: 0,
+            duration: 0.5,
+            delay: .2
+        })
+        gsap.from(brandName2.current, {
+            y: -90,
+            opacity: 0,
+            duration: 0.5,
+            delay: .2
+        })
+
+        gsap.from(infoLines, {
+            y: 50,
+            skewX: 20,
+            skewY: 1,
+            opacity: 0,
+            stagger: 1,
+            duration: 1,
+            delay: .2,
+            scrollTrigger: {
+                trigger: ".home-2",
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        gsap.from(infoBlock.current, {
+            x: -50,
+            opacity: 0,
+            stagger: 1,
+            duration: .5,
+            scrollTrigger: {
+                trigger: ".home-3-right",
+                start: "top 20%"
+            }
         })
 
     }, []);
@@ -36,28 +91,30 @@ const LandingPage = () => {
     return (
         <div className="home">
             <section className="home-1 wrapper">
-                <video src={video} autoPlay loop playsinline muted></video>
+                <video src={video} autoPlay loop playsinline muted ref={bgVideo}></video>
                 <div className="brand-name wrapper">
-                    <div className="brand-name-line-1 no-select">OCOVE</div>
-                    <div className="brand-name-line-2 no-select">STUDIOS</div>
+                    <div className="brand-name-line-1 no-select" ref={brandName1}>OCOVE</div>
+                    <div className="brand-name-line-2 no-select" ref={brandName2}>STUDIOS</div>
                 </div>
             </section>
 
             <section className="home-2 wrapper">
                 <div className="three-logo three-logo-model">
-                        <ThreeLogo />
-                        <div className="three-logo three-logo-overlay"></div>
+                    <ThreeLogo />
+                    <div className="three-logo three-logo-overlay"></div>
                 </div>
                 <div className="info-container-1">
-                    <p>We are a UK-based freelance collective specialising in website development,
-                        video production and 3D visualisation, providing innovative digital products
-                        for creatives, brands and businesses.</p>
+                    <p><div ref={infoLine1}>We are a UK-based freelance collective</div>
+                        <div ref={infoLine2}> specialising in website development,</div>
+                        <div ref={infoLine3}> video production and 3D visualisation,</div>
+                        <div ref={infoLine4}> providing innovative digital products</div>
+                        <div ref={infoLine5}> for creatives, brands and businesses.</div></p>
                 </div>
             </section>
 
             <section className="home-3 wrapper">
                 <div className="home-3-left">
-                    <div className="info-container-2">
+                    <div className="info-container-2" ref={infoBlock}>
                         <p>Working as a collective allows us to bring our best creative assets to projects.
                             Collaborating as individuals, we can make things together that we couldn't alone.</p>
                     </div>
@@ -75,7 +132,7 @@ const LandingPage = () => {
                                 fullScreen: {
                                     enable: false
                                 },
-                        fpsLimit: 60,
+                                fpsLimit: 60,
                                 interactivity: {
                                     events: {
                                         onClick: {
