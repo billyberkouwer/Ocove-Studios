c44bef5c31cb065d1c7790704b25ff90878782aa
Matt Congdon <mattcongdon95@gmail.com>
2022-01-11 11:58:20 +0000

    loading screen test

--- src/App.js
+++ src/App.js
@@ -1,67 +1,105 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Loading from './Loading.js';
 import Projects from './projects.json';
 import ProjectSection from './ProjectSection';
 import ServiceSection from './ServiceSection';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Transition, TransitionGroup, CSSTransition } from 'react-transition-group';
 import LandingPage from './LandingPage';
 import Navbar from './Navbar';
 
-function App() { 
-    
+function App() {
+
   /* --- COMPOSE PROJECTS DATA FROM JSON --- */
 
   const projectTitles = [];
   const projectText = [];
   const projectImages = [];
+  const projectCreditURLS1 = [];
+  const projectCredits1 = [];
+  const projectCreditURLS2 = [];
+  const projectCredits2 = [];
   const projectLinkURLS = [];
   const projectLinks = [];
-  
+
   (function () {
     for (var i in Projects) {
-        const p = Projects[i];
-        projectTitles.push(p.title);
-        projectText.push(p.text);
-        projectImages.push(p.images);
-        projectLinkURLS.push(p.links.url);
-        projectLinks.push(p.links.text);
-     };
+      const p = Projects[i];
+      projectTitles.push(p.title);
+      projectText.push(p.text);
+      projectImages.push(p.images);
+      projectCreditURLS1.push(p.credits1.url);
+      projectCredits1.push(p.credits1.text);
+      projectCreditURLS2.push(p.credits2.url);
+      projectCredits2.push(p.credits2.text);
+      projectLinkURLS.push(p.links.url);
+      projectLinks.push(p.links.text);
+    };
   })();
+
+  // LOADING HOOKS (TEMP)
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
 
 
   return (
-    <Router>
-      <div className="wrapper">
-        <Navbar />
-        <div className="global-container">
-          {/* <ScrollToTop /> */}
-          <Route render={({location}) => (
-            <TransitionGroup>
-              <CSSTransition key={location.key} timeout={300} classNames="fade">
-                <Switch location={location}>
-                  <Route exact path="/">
-                    <LandingPage />
-                  </Route>
-                  <Route path="/projects">
-                    <ProjectSection 
-                      projectTitles={projectTitles} 
-                      projectText={projectText} 
-                      projectImages={projectImages}
-                      projectLinkURLS={projectLinkURLS}
-                      projectLinks={projectLinks}
-                    />
-                  </Route>
-                  <Route path="/services">
-                    <ServiceSection />
-                  </Route>
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )} />
-        </div>
-      </div>
-    </Router>
+    <>
+      {
+        loading ?
+          <Transition>
+            <Loading />
+          </Transition>
+          :
+
+          <Router>
+            <div className="wrapper">
+              <Navbar />
+              <div className="global-container">
+                {/* <ScrollToTop /> */}
+                <Route render={({ location }) => (
+                  <TransitionGroup>
+                    <CSSTransition key={location.key} timeout={300} classNames="fade">
+                      <Switch location={location}>
+                        <Route exact path="/">
+                          <LandingPage />
+                        </Route>
+
+                        <Route path="/projects">
+                          <ProjectSection
+                            projectTitles={projectTitles}
+                            projectText={projectText}
+                            projectImages={projectImages}
+
+                            projectCreditURLS1={projectCreditURLS1}
+                            projectCredits1={projectCredits1}
+                            projectCreditURLS2={projectCreditURLS2}
+                            projectCredits2={projectCredits2}
+
+                            projectLinkURLS={projectLinkURLS}
+                            projectLinks={projectLinks}
+                          />
+                        </Route>
+                        <Route path="/services">
+                          <ServiceSection />
+                        </Route>
+                      </Switch>
+                    </CSSTransition>
+                  </TransitionGroup>
+                )} />
+              </div>
+            </div>
+          </Router>
+      }
+    </>
   );
+
 }
 
 export default App;
--- src/ProjectWindow.js
+++ src/ProjectWindow.js
@@ -1,24 +1,45 @@
 import { useEffect, useState, useRef } from "react";
 
-const ProjectWindow = ( props ) => {
+const ProjectWindow = (props) => {
 
     const projectTitles = props.projectTitles;
     const projectText = props.projectText;
     const project = props.project;
     const projectImages = props.projectImages;
+   
+    const projectCredits1 = props.projectCredits1;
+    const projectCreditURLS1 = props.projectCreditURLS1;
+    const projectCredits2 = props.projectCredits2;
+    const projectCreditURLS2 = props.projectCreditURLS2;
+
     const projectLinkURLS = props.projectLinkURLS;
-    const projectLinks= props.projectLinks;
+    const projectLinks = props.projectLinks;
 
     const [windowFade, setWindowFade] = useState('0');
     const [translation, setTranslation] = useState('translateX(5%)');
 
+
     const projectTitleObject = useRef(projectTitles[0]);
     const projectTextObject = useRef(projectText[0]);
+
+    const projectCreditsObject1 = useRef(projectCredits1[0])
+    const projectCreditURLSObject1 = useRef(projectCreditURLS1[0])
+    const projectCreditsObject2 = useRef(projectCredits2[0])
+    const projectCreditURLSObject2 = useRef(projectCreditURLS2[0])
+
     const projectLinkURLSObject = useRef(projectLinkURLS[0])
     const projectLinksObject = useRef(projectLinks[0])
 
+
     const projectTitleContent = Object.values(projectTitleObject);
     const projectTextContent = Object.values(projectTextObject);
+
+    const projectCreditsContent1 = Object.values(projectCreditsObject1);
+    const projectCreditURLSContent1 = Object.values(projectCreditURLSObject1);
+    const projectCreditsContent2 = Object.values(projectCreditsObject2);
+    const projectCreditURLSContent2 = Object.values(projectCreditURLSObject2);
+
+
     const projectLinkURLContent = Object.values(projectLinkURLSObject);
     const projectLinkContent = Object.values(projectLinksObject);
 
@@ -29,11 +50,17 @@ const ProjectWindow = ( props ) => {
     /* --- DYNAMICALLY SET PROJECT CONTENT FROM JSON --- */
 
     useEffect(() => {
-        setTimeout(function() {
+        setTimeout(function () {
             for (let i = 0; i < projectTitles.length; i++) {
                 if (project === i) {
                     projectTitleObject.current = projectTitles[i];
                     projectTextObject.current = projectText[i];
+
+                    projectCreditsObject1.current = projectCredits1[i];
+                    projectCreditURLSObject1.current = projectCreditURLS1[i];
+                    projectCreditsObject2.current = projectCredits2[i];
+                    projectCreditURLSObject2.current = projectCreditURLS2[i];
+
                     projectLinkURLSObject.current = projectLinkURLS[i];
                     projectLinksObject.current = projectLinks[i];
                 };
@@ -41,16 +68,16 @@ const ProjectWindow = ( props ) => {
             imageIteration.current = 0;
             setImage(projectImages[project][imageIteration.current]);
         }, 495);
-    },[projectTitleObject, projectTextObject, projectTitles, projectText, projectImages, project, projectLinkURLS, projectLinks]);
+    }, [projectTitleObject, projectTextObject, projectTitles, projectText, projectImages, project, projectCredits1, projectCreditURLS1, projectCredits2, projectCreditURLS2, projectLinkURLS, projectLinks]);
 
     console.log('refresh')
 
-    /* --- ANIMATE SELECT PROJECT --- */ 
+    /* --- ANIMATE SELECT PROJECT --- */
 
     useEffect(() => {
         setWindowFade('0');
         setTranslation('translateX(5%)');
-        setTimeout(function(){
+        setTimeout(function () {
             setImageFade('1');
             setWindowFade('1');
             setTranslation('translateX(0%)');
@@ -64,34 +91,42 @@ const ProjectWindow = ( props ) => {
             if (imageIteration.current >= projectImages[project].length - 1) {
                 imageIteration.current = 0;
                 setImageFade('0');
-                setTimeout(function(){
+                setTimeout(function () {
                     setImage(projectImages[project][imageIteration.current]);
                     setImageFade('1');
-                }, 500);      
-            }   else if (imageIteration.current < projectImages[project].length - 1) {
+                }, 500);
+            } else if (imageIteration.current < projectImages[project].length - 1) {
                 imageIteration.current++;
                 setImageFade('0');
-                setTimeout(function(){
+                setTimeout(function () {
                     setImage(projectImages[project][imageIteration.current]);
                     setImageFade('1');
                 }, 500);
             }
         }, 5000);
-        return () => {clearInterval(imageInterval)}
+        return () => { clearInterval(imageInterval) }
     }, [project, projectImages]);
 
-    return ( 
-        <div className="project-window" style={{opacity: windowFade, transform: translation}}>
+    return (
+        <div className="project-window" style={{ opacity: windowFade, transform: translation }}>
             <div className="project-text">
                 <h3>{projectTitleContent}</h3>
-                <p>{projectTextContent}</p>
-                <a href={projectLinkURLContent}>{projectLinkContent}</a>
+                <div className="project-text-body">
+                    <p className="project-description">{projectTextContent}</p>
+                </div>
+            </div>
+            <div className="project-footer">
+                <div className="project-credits" >
+                <a className="project-credits-1" target="_blank" rel="noreferrer" href={projectCreditURLSContent1}>{projectCreditsContent1}</a>
+                <a className="project-credits-2" target="_blank" rel="noreferrer" href={projectCreditURLSContent2}>{projectCreditsContent2}</a>
+                </div>
+                <a className="visit-project-link" target="_blank" rel="noreferrer" href={projectLinkURLContent}>{projectLinkContent}</a>
             </div>
             <div className="project-images-container">
-                <img className='project-images' style={{opacity: imageFade}} alt={`${projectTitleContent} images`} src={`./images/${image}`}></img>
+                <img className='project-images' style={{ opacity: imageFade }} alt={`${projectTitleContent} images`} src={`./images/${image}`}></img>
             </div>
         </div>
     );
 }
- 
+
 export default ProjectWindow;
